8f75b596537d560ee2a1684a08b5b433a821b3e9
F0xm0rtal <julien.bavoysi@epitech.eu>
2015-05-08 17:03:36 +0200

    health bar

---
+++ AquaTentacule/Assets/Script/health.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class health : MonoBehaviour {
+
+	public Scrollbar HealthBar;
+
+	void Damage (float value)
+	{
+		GameManager.instance.life -= value;
+		HealthBar.size = GameManager.instance.life / 100f;
+	}
+}

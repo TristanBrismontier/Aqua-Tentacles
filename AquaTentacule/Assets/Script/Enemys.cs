164f9ebf4e80a422d517e2ddb0c0c69ec69c775a
Darkham <thomas.picard666@gmail.com>
2015-05-08 18:01:45 +0200

    Enemys come to the Player

---
+++ AquaTentacule/Assets/Script/Enemys.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemys : MonoBehaviour {
+
+	public Transform Target;
+	//private GameObject Enemy;
+	private GameObject Player;
+	private float range;
+	public float speed;
+
+	void Start () {
+	}
+
+	void Update () {
+		transform.LookAt (Target.position);
+		transform.Rotate (new Vector3 (0, -90, 0), Space.Self);
+
+		if (Vector3.Distance (transform.position, Target.position) > 1f) {
+			transform.Translate (new Vector3 (speed * Time.deltaTime, 0, 0));
+		}
+	}
+}

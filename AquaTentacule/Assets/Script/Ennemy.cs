be8910af626e0dbafbef1468ecf6c300d08942a0
Tristan Brismontier <tristan.brismontier@gmail.com>
2015-05-08 10:46:38 +0200

    add Ennemy

--- AquaTentacule/Assets/Script/Ennemy.cs
+++ AquaTentacule/Assets/Script/Ennemy.cs
@@ -3,9 +3,20 @@ using System.Collections;
 
 public class Ennemy : MonoBehaviour {
 
+	public float nuttritionFact;
+	public float speed;
+
+	private Transform target;
+	private Rigidbody2D rb;
+	
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody2D>();
+		GameObject go = GameObject.FindGameObjectWithTag("Player");
+		target = go.transform;
+		Vector2 randomVector = Random.insideUnitCircle.Normalize();
+		Debug.Log (randomVector);
+		rb.velocity = new Vector2(randomVector.x*speed,randomVector.y*speed);
 	}
 	
 	// Update is called once per frame

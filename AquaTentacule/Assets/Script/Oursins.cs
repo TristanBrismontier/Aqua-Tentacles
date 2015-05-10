58a1a9aa83256f99c5688d27c5a743d6a54efee5
Darkham <thomas.picard666@gmail.com>
2015-05-10 11:16:53 +0200

    Oursins dégâts... ou pas ^^^

---
+++ AquaTentacule/Assets/Script/Oursins.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Oursins : MonoBehaviour {
+
+	void checkCollision(Collision2D other) {
+
+		if (other.gameObject.tag == "Player") {
+			GameManager.instance.looseLife(10);
+		}
+	}
+
+}

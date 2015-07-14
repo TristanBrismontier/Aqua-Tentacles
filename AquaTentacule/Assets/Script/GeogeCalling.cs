4532ec5d62879096288f73ba4e82ef37ce9ab449
Tristan Brismontier <tristan.brismontier@gmail.com>
2015-07-14 21:37:54 +0200

    - Cleaning - Calling George

---
+++ AquaTentacule/Assets/Script/GeogeCalling.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeogeCalling : MonoBehaviour {
+		
+		public Transform target;
+		public SpriteRenderer bubble; 
+		
+		void Start(){
+			bubble.enabled = false;
+		}
+		// Update is called once per frame
+		void Update () {
+			transform.position = target.position;
+			if (Input.GetKeyDown("space"))
+				StartCoroutine(showGeorgeBubble());
+		}
+		
+		public IEnumerator showGeorgeBubble(){
+			bubble.enabled = true;
+			yield return new WaitForSeconds(2);
+			bubble.enabled = false;
+		}
+}

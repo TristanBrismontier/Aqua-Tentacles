f98cb09f6bf15034751dcdabceb0e9a16f4fc05a
Tristan Brismontier <tristan.brismontier@gmail.com>
2015-07-14 13:34:58 +0200

    Fix Exit

--- AquaTentacule/Assets/Script/Labyrinth/LabyConfigTiles.cs
+++ AquaTentacule/Assets/Script/Labyrinth/LabyConfigTiles.cs
@@ -6,7 +6,7 @@ public class LabyConfigTiles{
 	public List<LabyTiles> getPreConfigTiles(){
 		List<LabyTiles> tiles = new List<LabyTiles>();
 
-		tiles.Add( new LabyTiles("FourDir",PlayerExit.North,PlayerExit.East,PlayerExit.South,PlayerExit.East));
+		tiles.Add( new LabyTiles("FourDir",PlayerExit.North,PlayerExit.East,PlayerExit.South,PlayerExit.West));
 		tiles.Add( new LabyTiles("EW",PlayerExit.East,PlayerExit.West));
 
 

bcdec6dd086781c5f9cdf4472a64d6498ef8bbf1
maudB <maud.boue@gmail.com>
2015-08-05 12:37:04 +0200

    modif labyrinthe (+ 1 tuile)

--- AquaTentacule/Assets/Script/Labyrinth/LabyConfigTiles.cs
+++ AquaTentacule/Assets/Script/Labyrinth/LabyConfigTiles.cs
@@ -6,12 +6,13 @@ public class LabyConfigTiles{
 	public List<LabyTiles> getPreConfigTiles(){
 		List<LabyTiles> tiles = new List<LabyTiles>();
 
-		tiles.Add( new LabyTiles("FourDir",PlayerExit.North,PlayerExit.East,PlayerExit.South,PlayerExit.West));
-		tiles.Add( new LabyTiles("EW",PlayerExit.East,PlayerExit.West));
+		//tiles.Add( new LabyTiles("FourDir",PlayerExit.North,PlayerExit.East,PlayerExit.South,PlayerExit.West));
+		//tiles.Add( new LabyTiles("EW",PlayerExit.East,PlayerExit.West));
 		tiles.Add( new LabyTiles("EWS",PlayerExit.East,PlayerExit.South,PlayerExit.West));
 		tiles.Add( new LabyTiles("FourDir2",PlayerExit.North,PlayerExit.East,PlayerExit.South,PlayerExit.West));
-		tiles.Add( new LabyTiles("EWN",PlayerExit.North,PlayerExit.East,PlayerExit.West));
+		tiles.Add( new LabyTiles("EWN",PlayerExit.East,PlayerExit.West));
 		tiles.Add( new LabyTiles("NSE",PlayerExit.North,PlayerExit.East,PlayerExit.South));
+		tiles.Add( new LabyTiles("NE",PlayerExit.North,PlayerExit.East));
 
 
 

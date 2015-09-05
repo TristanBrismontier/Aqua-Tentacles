fdf39b4b31de33f6fd708de56a18b9935fce7c99
Tristan Brismontier <tristan.brismontier@gmail.com>
2015-09-05 19:21:12 +0200

    Add DogSeaMeat

--- AquaTentacule/Assets/Script/Ennemy/DogSeaMeat.cs
+++ AquaTentacule/Assets/Script/Ennemy/DogSeaMeat.cs
@@ -1,11 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public class DogSeaMeat : MonoBehaviour {
 
-public class fish : MonoBehaviour {
-
-	
-	public int nutritionFact;
 	public float speedMax;
 	public float speedRotationMax;
 	
@@ -13,45 +10,49 @@ public class fish : MonoBehaviour {
 	private Rigidbody2D rb;
 	private float speed;
 	private bool face=true;
+	private float lastX;
+	private int jumpCount=0;
 
-
-
-	public float fireRate = 0.5F;
+	public float fireRate = 0.0001F;
 	private float nextFire = 0.0F;
-
-
+	
+	
 	// Use this for initialization
 	void Start () {
 		face = Random.Range(0,10) >= 5;
-		speed  = (float)(Random.Range(2,speedMax*12)/10);
+		speed  = 2f;
 		rb = GetComponent<Rigidbody2D>();
 		rb.freezeRotation =true;
 		GameObject go = GameObject.FindGameObjectWithTag("Player");
 		transform.localScale = new Vector3(face?3:-3,3, 1);
 		rb.velocity = new Vector2((face?-1:1)*speed ,rb.velocity.y);
+
 	}
-
+	
 	void FixedUpdate (){
-
+	
+		if ( Time.time > nextFire && Mathf.Abs(transform.position.x - lastX)<0.003f) {;
+			jumpCount++;
+			nextFire = Time.time + fireRate;
+			//Jump Script      
+			rb.AddForce (Vector2.up * 7, ForceMode2D.Impulse);
+			Debug.Log("#####Jump : + JumpCount : "+jumpCount);
+			if(jumpCount>15){
+				face =!face;
+				jumpCount=0;
+			}
+		}
 		transform.localScale = new Vector3(face?3:-3,3, 1);
 		rb.velocity = new Vector2((face?-1:1)*speed ,rb.velocity.y);
-
+		lastX = transform.position.x;
 	}
 	
 	void OnCollisionEnter2D(Collision2D coll) {
 		if (coll.gameObject.tag == "Player"){
-			GameManager.instance.eatFood(nutritionFact);
-			Destroy(this.gameObject);
+			GameManager.instance.death();
 			
 		}
-		if ( Time.time > nextFire) {
-			nextFire = Time.time + fireRate;
-			Debug.Log("Colision : " + face);
-			speed  = (float)(Random.Range(2,speedMax*12)/10);
-			face = !face;
-			Debug.Log("Colision : " + face);
-		}
-
+		
 		if(coll.gameObject.tag == "Octo"){
 			Destroy(this.gameObject);
 		}

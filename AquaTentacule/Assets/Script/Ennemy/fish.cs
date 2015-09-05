20198664fbc7b185a0f9e99bceffaf7e1e4d0c4c
Tristan Brismontier <tristan.brismontier@gmail.com>
2015-09-05 17:45:55 +0200

    Add Specials fish-food -PoissonRame -PoissonScargo
    Animate George.

---
+++ AquaTentacule/Assets/Script/Ennemy/fish.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class fish : MonoBehaviour {
+
+	
+	public int nutritionFact;
+	public float speedMax;
+	public float speedRotationMax;
+	
+	private Transform target;
+	private Rigidbody2D rb;
+	private float speed;
+	private bool face=true;
+
+
+	public float fireRate = 0.5F;
+	private float nextFire = 0.0F;
+
+
+	// Use this for initialization
+	void Start () {
+		face = Random.Range(0,10) >= 5;
+		speed  = (float)(Random.Range(2,speedMax*12)/10);
+		rb = GetComponent<Rigidbody2D>();
+		rb.freezeRotation =true;
+		GameObject go = GameObject.FindGameObjectWithTag("Player");
+		transform.localScale = new Vector3(face?3:-3,3, 1);
+		rb.velocity = new Vector2((face?-1:1)*speed ,rb.velocity.y);
+	}
+
+	void FixedUpdate (){
+		transform.localScale = new Vector3(face?3:-3,3, 1);
+		rb.velocity = new Vector2((face?-1:1)*speed ,rb.velocity.y);
+	}
+	
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Player"){
+			GameManager.instance.eatFood(nutritionFact);
+			Destroy(this.gameObject);
+			
+		}
+		if ( Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Debug.Log("Colision : " + face);
+			speed  = (float)(Random.Range(2,speedMax*12)/10);
+			face = !face;
+			Debug.Log("Colision : " + face);
+		}
+
+		if(coll.gameObject.tag == "Octo"){
+			Destroy(this.gameObject);
+		}
+		
+	}
+}
--- AquaTentacule/Assets/Script/GameManager.cs
+++ AquaTentacule/Assets/Script/GameManager.cs
@@ -2,10 +2,10 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 	
 	public static GameManager instance = null;
-
 	public float life;
 	public float starving = 2.0f;
 	public Transform startPosition;
@@ -16,188 +16,201 @@ public class GameManager : MonoBehaviour {
 	public int maxRange;
 	public int limiteVieHeartbeat;
 	public bool debug;
-
-	public PlayerInfo playerInfo = new PlayerInfo();
+	public PlayerInfo playerInfo = new PlayerInfo ();
 	public Bounds cameraBound;
 	public GameObject[] foods;
 	public GameObject Octopus;
 	public GameObject FishEye;
 	public GameObject bubbleExplosionPlop;
 	private Player player;
-	private List<GameObject> instanciatesGameObjects = new List<GameObject>();
-
+	private List<GameObject> instanciatesGameObjects = new List<GameObject> ();
 	private bool canDie;
 	private bool isPlayingSound;
 
-	void Awake () {
-		if (instance == null){
+	void Awake ()
+	{
+		if (instance == null) {
 			Debug.Log ("New Instance GM");
 			instance = this;
+		} else if (instance != this) {
+			Destroy (gameObject);
 		}
-		else if(instance != this)
-		{
-			Destroy(gameObject);
-		}
-		DontDestroyOnLoad(gameObject);
+		DontDestroyOnLoad (gameObject);
 	}
 
-	public void Start(){
+	public void Start ()
+	{
 		isPlayingSound = false;
-		initGame();
+		initGame ();
 	}
 
-	public void setPlayer(Player _player){
+	public void setPlayer (Player _player)
+	{
 		player = _player;
-		playerInfo.restorePlayerInfo(player);
-		startPosition.position = new Vector3(player.transform.position.x,player.transform.position.y,player.transform.position.z);
+		playerInfo.restorePlayerInfo (player);
+		startPosition.position = new Vector3 (player.transform.position.x, player.transform.position.y, player.transform.position.z);
 	}
 
-	public void initPlayerPosition(Transform position){
+	public void initPlayerPosition (Transform position)
+	{
 		playerInfo.startDeltaPosition = position.position;
 	}
 
-	public void initGame(){
+	public void initGame ()
+	{
 		playerInfo.playerHasTenta = false;
 		canDie = true;
 		playerInfo.tentaCount = 0;
-		if(!debug){
-			Debug.Log("Deb "+debug);
-			initAntagonist();
-		}
-			
+		initAntagonist ();
+
 	}
 
-	public void initAntagonist(){
-		int foodNumber = Random.Range(minFood,maxFood);
-		for(int i = 0 ; i<foodNumber; i++){
-			AddFood();
-		}
+	public void initAntagonist ()
+	{
+		if (!debug) {
+			Debug.Log ("Deb " + debug);
+			int foodNumber = Random.Range (minFood, maxFood);
+			for (int i = 0; i<foodNumber; i++) {
+				AddFood ();
+			}
 		
-		for(int i = 0 ; i<octopus; i++){
-			RespawnOctopus();
-		}
+			for (int i = 0; i<octopus; i++) {
+				RespawnOctopus ();
+			}
 		
-		for(int i = 0 ; i<FishCount; i++){
-			RespawnFishEye();
+			for (int i = 0; i<FishCount; i++) {
+				RespawnFishEye ();
+			}
 		}
 	}
 
-	public void AddFood(){
-		GameObject foodChoice = foods[Random.Range (0, foods.Length)];
-		GameObject go = Instantiate(foodChoice,getLimitedRandomPos(), Quaternion.identity)as GameObject;
-		instanciatesGameObjects.Add(go);
+	public void AddFood ()
+	{
+		GameObject foodChoice = foods [Random.Range (0, foods.Length)];
+		GameObject go = Instantiate (foodChoice, getLimitedRandomPos (), Quaternion.identity)as GameObject;
+		instanciatesGameObjects.Add (go);
 	}
 	
-	public void RespawnOctopus(){
-		GameObject go = Instantiate(Octopus, getLimitedRandomPos(), Quaternion.identity) as GameObject;
-		instanciatesGameObjects.Add(go);
+	public void RespawnOctopus ()
+	{
+		GameObject go = Instantiate (Octopus, getLimitedRandomPos (), Quaternion.identity) as GameObject;
+		instanciatesGameObjects.Add (go);
 	}
 
-	public void RespawnFishEye(){
-		GameObject go = Instantiate(FishEye, getLimitedRandomPos(), Quaternion.identity) as GameObject;
-		instanciatesGameObjects.Add(go);
+	public void RespawnFishEye ()
+	{
+		GameObject go = Instantiate (FishEye, getLimitedRandomPos (), Quaternion.identity) as GameObject;
+		instanciatesGameObjects.Add (go);
 	}
 
-	private Vector3 getLimitedRandomPos(){
-		float x = (float)(Random.Range(-maxRange,maxRange));
-		float y = (float)(Random.Range(-maxRange,maxRange));
-		GameObject player = GameObject.FindGameObjectWithTag("Player");
-		float deltaX = x  - player.transform.position.x;
-		float deltaY = y  - player.transform.position.y;
-		if( Mathf.Abs(deltaX) < 3.1f ){
-			x=x+((deltaX<=0)?-3:3);
+	private Vector3 getLimitedRandomPos ()
+	{
+		float x = (float)(Random.Range (-maxRange, maxRange));
+		float y = (float)(Random.Range (-maxRange, maxRange));
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+		float deltaX = x - player.transform.position.x;
+		float deltaY = y - player.transform.position.y;
+		if (Mathf.Abs (deltaX) < 3.1f) {
+			x = x + ((deltaX <= 0) ? -3 : 3);
 		}
-		if( Mathf.Abs(deltaY) < 3.1f ){
-			y=y+((deltaY<=0)?-3:3);
+		if (Mathf.Abs (deltaY) < 3.1f) {
+			y = y + ((deltaY <= 0) ? -3 : 3);
 		}
-		return new Vector3( x, y, 0);
+		return new Vector3 (x, y, 0);
 	}
 
-	public void eatFood(int nutritionFact){
+	public void eatFood (int nutritionFact)
+	{
 		AddFood ();
 		eat (nutritionFact);
-		SoundManager.instance.RandomizeSfx(SoundManager.instance.efxSource, SoundManager.instance.eatSounds);
+		SoundManager.instance.RandomizeSfx (SoundManager.instance.efxSource, SoundManager.instance.eatSounds);
 	}
-	public void eatOctoPus(int nutritionFact){
-		RespawnOctopus();
-		player.spawnTenta();
+
+	public void eatOctoPus (int nutritionFact)
+	{
+		RespawnOctopus ();
+		player.spawnTenta ();
 		playerInfo.playerHasTenta = true;
 		eat (nutritionFact);
 	}
-	public void eatFishEye(int nutritionFact){
-		RespawnFishEye();
-		player.spawnEye();
+
+	public void eatFishEye (int nutritionFact)
+	{
+		RespawnFishEye ();
+		player.spawnEye ();
 		playerInfo.playerHasEye = true;
 		eat (nutritionFact);
 	}
 
-	public void looseLife(int damage){
-		eat (damage*-1);
-		player.Hurt();
+	public void looseLife (int damage)
+	{
+		eat (damage * -1);
+		player.Hurt ();
 	}
 
-	private void eat(int nutritionFact){
+	private void eat (int nutritionFact)
+	{
 		life = life + nutritionFact;
-		if(nutritionFact > 0){
-			player.Eat();
+		if (nutritionFact > 0) {
+			player.Eat ();
 		}
 	}
 
-	void Update () {
+	void Update ()
+	{
 		//Life max = 150 pv
 		if (life > 150) {
 			//life = 150;
 		}
 		//Each seconds = -2 pv
 		life -= starving * Time.deltaTime;
-		if(life<=0){
-			death();
+		if (life <= 0) {
+			death ();
 		}
 
-		if(life <=limiteVieHeartbeat && !isPlayingSound)
-		{
+		if (life <= limiteVieHeartbeat && !isPlayingSound) {
 			isPlayingSound = true;
-			SoundManager.instance.PlaySingleLoop(SoundManager.instance.heartbeatSound, SoundManager.instance.interfaceSource, 0.3f);
-			StartCoroutine(SoundManager.instance.FadeIn(SoundManager.instance.interfaceSource, 0.3f));
-		}
-		else if(life >limiteVieHeartbeat && isPlayingSound)
-		{
+			SoundManager.instance.PlaySingleLoop (SoundManager.instance.heartbeatSound, SoundManager.instance.interfaceSource, 0.3f);
+			StartCoroutine (SoundManager.instance.FadeIn (SoundManager.instance.interfaceSource, 0.3f));
+		} else if (life > limiteVieHeartbeat && isPlayingSound) {
 			isPlayingSound = false;
-			StartCoroutine(SoundManager.instance.FadeOut(SoundManager.instance.interfaceSource,0.3f));
+			StartCoroutine (SoundManager.instance.FadeOut (SoundManager.instance.interfaceSource, 0.3f));
 		}
 	}
 	
-	public void loadLevel(string name){
-		playerInfo.saveInfo(player);
-		Application.LoadLevel(name);
+	public void loadLevel (string name)
+	{
+		playerInfo.saveInfo (player);
+		Application.LoadLevel (name);
 	}
 
-	public void death(){
-		if(canDie){
+	public void death ()
+	{
+		if (canDie) {
 			canDie = false;
 			life = 100000;
-			SoundManager.instance.RandomizeSfx(SoundManager.instance.efxSource,SoundManager.instance.deadSounds);
-			GameObject playerGO = GameObject.FindGameObjectWithTag("Player");
-			playerGO.transform.localScale = new Vector3(0f,0f,0f);
-			GameObject buble = Instantiate(bubbleExplosionPlop,new Vector3( playerGO.transform.position.x,playerGO.transform.position.y,playerGO.transform.position.z-1), playerGO.transform.rotation) as GameObject;
-			StartCoroutine(deadPlayer(buble));
+			SoundManager.instance.RandomizeSfx (SoundManager.instance.efxSource, SoundManager.instance.deadSounds);
+			GameObject playerGO = GameObject.FindGameObjectWithTag ("Player");
+			playerGO.transform.localScale = new Vector3 (0f, 0f, 0f);
+			GameObject buble = Instantiate (bubbleExplosionPlop, new Vector3 (playerGO.transform.position.x, playerGO.transform.position.y, playerGO.transform.position.z - 1), playerGO.transform.rotation) as GameObject;
+			StartCoroutine (deadPlayer (buble));
 		}
 	}
 
-	public IEnumerator deadPlayer(GameObject buble){
-		yield return new WaitForSeconds(2);
+	public IEnumerator deadPlayer (GameObject buble)
+	{
+		yield return new WaitForSeconds (2);
 	
-		Destroy(buble);
-		GameObject playerGO = GameObject.FindGameObjectWithTag("Player");
-		playerGO.SetActive(true);
-		playerGO.transform.localScale = new Vector3(2.3f,2.3f,2.3f);
+		Destroy (buble);
+		GameObject playerGO = GameObject.FindGameObjectWithTag ("Player");
+		playerGO.SetActive (true);
+		playerGO.transform.localScale = new Vector3 (2.3f, 2.3f, 2.3f);
 		player.transform.position = startPosition.position;
-		yield return new WaitForSeconds(0.5f);
-		player.resetPlayer();
-		foreach (GameObject gobj in instanciatesGameObjects)
-		{
-			Destroy(gobj);
+		yield return new WaitForSeconds (0.5f);
+		player.resetPlayer ();
+		foreach (GameObject gobj in instanciatesGameObjects) {
+			Destroy (gobj);
 		}
-		initGame();
+		initGame ();
 	}
 }

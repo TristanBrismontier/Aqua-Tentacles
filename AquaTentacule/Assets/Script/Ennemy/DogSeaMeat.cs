eb577437d236ac7213ca9e71b27aa673b7aa9de8
Tristan Brismontier <tristan.brismontier@gmail.com>
2015-09-06 15:55:43 +0200

    Better DogSeaMeat

--- AquaTentacule/Assets/Script/Ennemy/DogSeaMeat.cs
+++ AquaTentacule/Assets/Script/Ennemy/DogSeaMeat.cs
@@ -5,7 +5,8 @@ public class DogSeaMeat : MonoBehaviour {
 
 	public float speedMax;
 	public float speedRotationMax;
-	
+
+	public float dogScale=6F;
 	private Transform target;
 	private Rigidbody2D rb;
 	private float speed;
@@ -13,36 +14,37 @@ public class DogSeaMeat : MonoBehaviour {
 	private float lastX;
 	private int jumpCount=0;
 
-	public float fireRate = 0.0001F;
+	private float fireJump = 3.0F;
 	private float nextFire = 0.0F;
 	
 	
 	// Use this for initialization
 	void Start () {
+		nextFire = Time.time;
 		face = Random.Range(0,10) >= 5;
-		speed  = 2f;
+		speed  = 3f;
 		rb = GetComponent<Rigidbody2D>();
 		rb.freezeRotation =true;
 		GameObject go = GameObject.FindGameObjectWithTag("Player");
-		transform.localScale = new Vector3(face?3:-3,3, 1);
+		transform.localScale = new Vector3(face?dogScale:-dogScale,dogScale, 1);
 		rb.velocity = new Vector2((face?-1:1)*speed ,rb.velocity.y);
 
 	}
 	
 	void FixedUpdate (){
 	
-		if ( Time.time > nextFire && Mathf.Abs(transform.position.x - lastX)<0.003f) {;
+		if ( (Time.time > nextFire) && Mathf.Abs(transform.position.x - lastX)<0.003f) {;
 			jumpCount++;
-			nextFire = Time.time + fireRate;
+			nextFire = Time.time + fireJump;
 			//Jump Script      
 			rb.AddForce (Vector2.up * 7, ForceMode2D.Impulse);
-			Debug.Log("#####Jump : + JumpCount : "+jumpCount);
-			if(jumpCount>15){
+			Debug.Log("#####Jump : + JumpCount : "+jumpCount+" Time.time ! "+Time.time+" > "+nextFire+" "+fireJump);
+			if(jumpCount>4){
 				face =!face;
 				jumpCount=0;
 			}
 		}
-		transform.localScale = new Vector3(face?3:-3,3, 1);
+		transform.localScale = new Vector3(face?dogScale:-dogScale,dogScale, 1);
 		rb.velocity = new Vector2((face?-1:1)*speed ,rb.velocity.y);
 		lastX = transform.position.x;
 	}

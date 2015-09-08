12f54e82c9cc67d2be3e9f277108cd19bdb0ecb6
maudB <maud.boue@gmail.com>
2015-09-08 16:11:00 +0200

    +1 tuile laby

--- AquaTentacule/Assets/Script/Labyrinth/LabyConfigTiles.cs
+++ AquaTentacule/Assets/Script/Labyrinth/LabyConfigTiles.cs
@@ -12,7 +12,7 @@ public class LabyConfigTiles{
 		tiles.Add( new LabyTiles("NE",PlayerExit.North,PlayerExit.East));
 		tiles.Add( new LabyTiles("WMachinesLumBleues",PlayerExit.West));
 		tiles.Add( new LabyTiles("NSE2",PlayerExit.North,PlayerExit.East,PlayerExit.South));
-
+		tiles.Add( new LabyTiles("NWEfondMachines",PlayerExit.North,PlayerExit.East,PlayerExit.West));
 		return tiles;
 		
 	}
